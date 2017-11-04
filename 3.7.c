5640854fc68498c9826cd956a8626b895fcc1cd9
sidjuli <32111124+sidjuli@users.noreply.github.com>
2017-11-04 18:00:37 +0300

    Create 3.7.c

---
+++ 3.7.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int i, j, n, m;
+    const int c=0, k=1;
+    scanf("%d", &n);
+    scanf("%d", &m);
+    int* ost = (int*)malloc(n*sizeof(int));
+    for (i=0; i<n; i++)
+    {
+        ost[i]=1;
+    }
+    for (i=0; i<n; i++)
+    {
+        for (j=0; j<m; j++)
+        {
+            while (ost[c] != 1)
+            {
+                c =(c+1)%n;
+            }
+        }
+        ost[c]=0;
+        if (i>=n-3)
+            {
+                printf("%d",c);
+            }
+    }
+}
