131f3c2bf153bfecf5d2f628c22d06f98935890b
sidjuli <32111124+sidjuli@users.noreply.github.com>
2017-11-04 17:59:38 +0300

    Create 3.5.c

---
+++ 3.5.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    int n, i, k;
+    scanf("%d", &n);
+    k=0;
+    for (i=2; i<=sqrt(n); i++)
+    {
+        if (n%i==0)
+        {
+            k=k+1;
+        }
+    }
+    if (k=0)
+    {
+        printf("chislo prostoe");
+    }
+    else
+    {
+        printf("chislo ne prostoe");
+    }
+}
