099f335c080444fca9cd4a871f495598cc0d3dd6
sidjuli <32111124+sidjuli@users.noreply.github.com>
2017-12-15 12:44:43 +0300

    Update 3.5.c

--- 3.5.c
+++ 3.5.c
@@ -3,22 +3,23 @@
 
 int main()
 {
-    int n, i, k;
-    scanf("%d", &n);
-    k=0;
-    for (i=2; i<=sqrt(n); i++)
-    {
-        if (n%i==0)
-        {
-            k=k+1;
-        }
-    }
-    if (k=0)
-    {
-        printf("chislo prostoe");
-    }
-    else
-    {
-        printf("chislo ne prostoe");
-    }
+int n, i, k;
+scanf("%d", &n);
+k=0;
+for (i=2; i<=sqrt(n); i++)
+{
+if (n%i==0)
+{
+k++;
+printf("%d\n", k);
+}
+}
+if (k==0)
+{
+printf("chislo prostoe");
+}
+else
+{
+printf("chislo ne prostoe");
+}
 }
